78a2890bf573c0565fc687e706d92e7e38a07317
Maryna Yabluchanska <marina.iabluchanskaya@gmail.com>
2022-11-08 13:31:29 +0100

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+let path = require('path');
 
+(function copyDir() {
+  return fsPromises.mkdir('files-copy', {recursive: true})
+    .then(() => {
+        fsPromises.readdir(path.join(__dirname, 'files'), {withFileTypes: true})
+          .then((result) => {
+              let oldFolderNames = [];
+              result.forEach(
+                (file) => {
+                  if (file.isFile()) {
+                    oldFolderNames.push(file.name);
+                    fsPromises.copyFile(path.join(__dirname, 'files/' + file.name), path.join(__dirname, 'files-copy/' + file.name));
+                  }
+                }
+              )
+              return oldFolderNames;
+            }
+          )
+          .then((oldFolder) => {
+            fsPromises.readdir(path.join(__dirname, 'files-copy')).then((newFolder) => {
+              newFolder.map((fileName) => {
+                if (!oldFolder.includes(fileName)) {
+                  fs.unlink((path.join(__dirname, 'files-copy/' + fileName)), () => {
+                    console.log('File successfully deleted ' + fileName);
+                  });
+                }
+              })
+            });
+          })
+      }
+    )
+}());
